d37476408726f6df245598a544923953089b0bf6
Zicophs <51864963+Zicophs@users.noreply.github.com>
2024-04-07 16:04:03 -0300

    Cambios en Pom y demo birthday

---
+++ tp2-nico-seccion-1/src/test/java/com/example/tp2nicoseccion1/Tp2NicoSeccion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp2nicoseccion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2NicoSeccion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
