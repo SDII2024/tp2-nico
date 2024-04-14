3678c7564dfcb1c64ae0849a9fb0033c03bbb76f
Zicophs <51864963+Zicophs@users.noreply.github.com>
2024-04-14 18:06:57 -0300

    Ej 1,2 y 3

--- tp2-nico-seccion-1/src/main/java/com/example/tp2nicoseccion1/ej1/Tp2NicoSeccion1Application.java
+++ tp2-nico-seccion-1/src/main/java/com/example/tp2nicoseccion1/ej1/Tp2NicoSeccion1Application.java
@@ -1,4 +1,4 @@
-package com.example.tp2nicoseccion1;
+package com.example.tp2nicoseccion1.ej1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
